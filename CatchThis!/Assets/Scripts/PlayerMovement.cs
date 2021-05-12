44f80faea1885f03b4e313c765580b2023be0067
popescuedi99 <popescuedi99@yahoo.com>
2021-05-12 19:08:01 +0300

    Finished player movement animation

--- CatchThis!/Assets/Scripts/PlayerMovement.cs
+++ CatchThis!/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,18 @@ public class PlayerMovement : Photon.MonoBehaviour
     public PhotonView phView;
     public static int bombsAvailable = 0;
     public Text playerName;
+    public SpriteRenderer sr;
+
     [SerializeField]
     private GameObject bombPrefab;
+
+    Animator animator;
 
     // Use this for initialization
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         if (phView.isMine || !PhotonNetwork.connected)
         {
             bombsAvailable = 1;
@@ -43,21 +49,30 @@ public class PlayerMovement : Photon.MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+            sr.flipX = false;
+            animator.SetBool("IsWalkingSide", true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+            sr.flipX = true;
+            animator.SetBool("IsWalkingSide", true);
         }
+        else animator.SetBool("IsWalkingSide", false);
 
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
+            animator.SetBool("IsWalkingUp", true);
         }
+        else animator.SetBool("IsWalkingUp", false);
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.down * movementspeed * Time.deltaTime);
+            animator.SetBool("IsWalkingDown", true);
         }
+        else animator.SetBool("IsWalkingDown", false);
 
         if (Input.GetKeyDown("space") && bombsAvailable > 0)
         {
